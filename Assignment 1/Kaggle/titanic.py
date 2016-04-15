522482f44d5d3cd4061eeff1e900828d91c6de18
Jan Westerdiep <janner@gmail.com>
2016-04-15 15:51:19 +0200

    werkt nog steeds slecht

--- Assignment 1/Kaggle/titanic.py
+++ Assignment 1/Kaggle/titanic.py
@@ -1,51 +1,118 @@
+from __future__ import division
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
+from collections import Counter
+from sklearn import cross_validation, metrics
+from sklearn.cross_validation import train_test_split
 
-def convertSex(df):
-    df['Sex'] = df['Sex'].map(lambda x: x == 'male').astype(int)
+# fill the NaN-values in the given column with the non-NaN-median
+def fillMedian(df, col, medianGiven = None):
+    if medianGiven is None:
+        df[col] = df[col].fillna(df[col].median())
+    else:
+        df[col] = df[col].fillna(medianGiven)
 
-def fillAge(df):
-    median_ages = np.zeros((2,3))
+# fill the NaN-values in the given column with the non-NaN-median matrix 
+# separated by (sex, pclass)
+def fillMedianClassSex(df, col, mediansGiven = None):
+    if mediansGiven is None:
+        medians = np.zeros((2,3))
+        for i in range(0, 2):
+            for j in range(0, 3):
+                medians[i,j] = df[(df.Sex == i) & (df.Pclass == j+1)][col].dropna().median()
+    else:
+        medians = mediansGiven
+
     for i in range(0, 2):
         for j in range(0, 3):
-            median_ages[i,j] = df[(df.Sex == i) & (df.Pclass == j+1)]['Age'].dropna().median()
+            df.loc[ (df[col].isnull()) & (df.Sex == i) & (df.Pclass == j+1), col] = medians[i,j]
 
-    for i in range(0, 2):
-        for j in range(0, 3):
-            df.loc[ (df.Age.isnull()) & (df.Sex == i) & (df.Pclass == j+1), 'Age'] = median_ages[i,j]
+    return medians
 
-def fillFare(df):
-    median_fares = np.zeros((2,3))
-    for i in range(0, 2):
-        for j in range(0, 3):
-            median_fares[i,j] = df[(df.Sex == i) & (df.Pclass == j+1)]['Fare'].dropna().median()
+# fill a column with given value
+def fillWith(df, col, fillVal):
+    assert( fillVal != None)
+    df[col] = df[col].fillna(fillVal)
 
-    for i in range(0, 2):
-        for j in range(0, 3):
-            df.loc[ (df.Fare.isnull()) & (df.Sex == i) & (df.Pclass == j+1), 'Fare'] = median_fares[i,j]
+# fill the given column using the given method
+def fillColumn(df, col, method, fillVal = None):
+    if method == 0:
+        return fillMedian(df, col, fillVal)
+    elif method == 1:
+        return fillMedianClassSex(df, col, fillVal)
+    elif method == 2:
+        assert(fillVal != None)
+        return fillWith(df, col, fillVal)
+    else:
+        assert(False)
 
+def convertToOneHot(df, col):
+    pass
+
+def convertToInt(df, col, dctGiven = None):
+    if dctGiven is None:
+        dct = {x:k for k,x in enumerate(df[col].unique())}
+    else:
+        dct = dctGiven
+
+    df[col] = df[col].map(dct)
+
+    return dct
+
+# read stuff
 dft = pd.read_csv('test.csv', header=0)
 df = pd.read_csv('train.csv', header=0)
 
-convertSex(df)
-convertSex(dft)
-fillAge(df)
-fillAge(dft)
-fillFare(df)
-fillFare(dft)
+""" Data enchrichment phase """
 
-predictors = ['Pclass', 'SibSp', 'Parch', 'Sex', 'Age', 'Fare']
+# fill Age and Fare NaN-values
+mediansAge = fillColumn(df, 'Age', 0)
+fillColumn(dft, 'Age', 0, mediansAge)
+mediansFare = fillColumn(df, 'Fare', 0)
+fillColumn(dft, 'Fare', 0, mediansFare)
 
+# fill the missing embarked values with the most-occuring value
+fillColumn(df, 'Embarked', 2, sorted(Counter(df['Embarked'].values).items(), key=lambda x: x[1])[-1][0])
+
+# convert some stuff
+dctSex = convertToInt(df, 'Sex')
+convertToInt(dft, 'Sex', dctSex)
+dctEmb = convertToInt(df, 'Embarked')
+convertToInt(dft, 'Embarked', dctEmb)
+
+df['FamilySize'] = df['SibSp'] + df['Parch']
+dft['FamilySize'] = dft['SibSp'] + dft['Parch']
+
+# choose which columns to predict with
+predictors = ['Pclass', 'SibSp', 'Parch', 'Sex', 'Age', 'Fare', 'Embarked', 'FamilySize']
+
+""" Training phase """
+
+traindata = df[predictors].values
+targets = df.Survived
+
+Xtr, Xte, ytr, yte = train_test_split( traindata, targets, test_size = 1/3, random_state=42)
+print 
+
+# train classifier
 forest = RandomForestClassifier(n_estimators = 100)
-forest.fit(df[predictors].values, df['Survived'])
+"""
+scores = cross_validation.cross_val_score(forest, df[predictors].values, df.Survived, cv=5)
+print scores.mean()
+"""
 
+forest.fit(Xtr, ytr)
+predictions = forest.predict(Xte)
+print metrics.classification_report(yte, predictions)
+
+""" Testing phase """
+
+# predict final output
 output = forest.predict(dft[predictors].values)
 output = (output > 0.5).astype(int)
 
-submission = pd.DataFrame({
-    'PassengerId': dft['PassengerId'], 'Survived': output
-});
+submission = pd.DataFrame({ 'PassengerId': dft['PassengerId'], 'Survived': output });
 
-submission.to_csv('simplemodel.csv', index=False)
+submission.to_csv('simplemodel_embarked.csv', index=False)
